748f23b925b0f065b4e13b45f48f98a56e69c99b
[phil sellick] <[psellick76@gmail.com]>
2023-09-14 22:09:55 -0400

    Fixed results page to get form, ideas for logics

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-hour = []
+hour = ["Deep dive whatshouldido app! :]"]
 thirty_min = ["Code wars (hard)", "Code Wars (Easy)"]
-fiften_min = []
-five_min = []
+fifteen_min = ["Code wars (medium)"]
+five_min = ["Code wars (easy)"]
 
-study_time = 0
 time_left = 0
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    if request.method == "POST":
-        study_time, time_left = request.form.get("content")
-        if time_left > 120:
-            print("wow that's a lot of time to study. Here are some of the things you could do:")
-        elif time_left < 120 and study_time > 60:
-            time_left = study_time - 60
-            print("hard or long stuff")
-
     return render_template("home.html")
+
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    study_time = request.form.get("time-mins")
+    time_left = study_time
+    if time_left > 120:
+        #print("You could do " + random.choice(hour) + ", " + random.choice(thirty_min) + ", " + "and some small stuff like " + random.choice(fifteen_min) + ".")
+       # print paragraph("wow that's a lot of time to study. Here are some of the things you could do:")
+    elif time_left < 120 and study_time > 60:
+        time_left = study_time - 60
+        print("hard or long stuff")
+    return render_template("results.html", study_time = study_time)
