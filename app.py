7ddbbb7ff77e5c48f8b8a9e7c7a26afe29724987
[phil sellick] <[psellick76@gmail.com]>
2023-09-28 21:37:16 -0400

    Vertical slice! logic solidified in app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import random
 
 app = Flask(__name__)
 
-hour = ["Deep dive whatshouldido app! :]"]
-thirty_min = ["Code wars (hard)", "Code Wars (Easy)"]
-fifteen_min = ["Code wars (medium)"]
-five_min = ["Code wars (easy)"]
+hour = ["Deep dive whatshouldido app!", "Data Stuctures problems."]
+thirty_min = ["Code wars (hard)", "flask bootcamp video with practice afterwards"]
+fifteen_min = ["Code wars (medium)", "a flask tutorial video", "a django tutorial video"]
+five_min = ["Code wars (easy)", "short tutorial video", "look up something you don't know"]
 
 time_left = 0
 
@@ -16,12 +16,12 @@ def home():
 
 @app.route("/results", methods=["GET", "POST"])
 def results():
-    study_time = request.form.get("time-mins")
-    time_left = study_time
-    if time_left > 120:
-        #print("You could do " + random.choice(hour) + ", " + random.choice(thirty_min) + ", " + "and some small stuff like " + random.choice(fifteen_min) + ".")
-       # print paragraph("wow that's a lot of time to study. Here are some of the things you could do:")
-    elif time_left < 120 and study_time > 60:
-        time_left = study_time - 60
+    study_time = int(request.form.get("time-mins"))
+    if study_time > 120:
+        time_left = study_time - 120
+        result = ("You could do " + random.choice(hour) + ", " + random.choice(thirty_min) + ", " + "and some small stuff like " + random.choice(fifteen_min) + ". " + 
+                  "You would still have " + str(time_left) + " minutes left!")
+    elif study_time < 120 and study_time > 60:
+        study_time -= 60
         print("hard or long stuff")
-    return render_template("results.html", study_time = study_time)
+    return render_template("results.html", study_time = study_time, result = result)
